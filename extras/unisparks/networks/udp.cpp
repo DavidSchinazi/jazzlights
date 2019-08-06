1c134b08df95edef2cacbca08b7c8e8fddd5f177
David Schinazi <david.schinazi@gmail.com>
2019-08-05 19:58:55 -0700

    Refactor UDP code

--- extras/unisparks/networks/udp.cpp
+++ extras/unisparks/networks/udp.cpp
@@ -5,7 +5,6 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <netdb.h>
-#include <netinet/in.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
@@ -20,58 +19,66 @@
 namespace unisparks {
 
 
+Udp::Udp(int p, const char* addr) : port_(static_cast<uint16_t>(p)), fd_(-1) {
+  assert(strnlen(addr, sizeof(mcastAddrStr_) + 1) < sizeof(mcastAddrStr_));
+  strncpy(mcastAddrStr_, addr, sizeof(mcastAddrStr_));
+  int parsed = inet_aton(addr, &mcastAddr_);
+  assert(parsed == 1);
+}
+
+
 NetworkStatus Udp::update(NetworkStatus status) {
   if (status == INITIALIZING || status == CONNECTING) {
     int optval, flags;
     sockaddr_in if_addr;
     struct ip_mreq mc_group;
 
-    assert(fd == 0); // must be true if status == connecting
+    assert(fd_ < 0); // must be true if status == connecting
 
-    fd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (fd < 0) {
+    fd_ = socket(AF_INET, SOCK_DGRAM, 0);
+    if (fd_ < 0) {
       error("Can't create client UDP socket");
       goto err;
     }
 
     optval = 1;
-    if (setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval)) < 0) {
-      error("Can't set reuseaddr option on UDP socket %d: %s", fd, strerror(errno));
+    if (setsockopt(fd_, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval)) < 0) {
+      error("Can't set reuseaddr option on UDP socket %d: %s", fd_, strerror(errno));
       goto err;
     }
 
-    flags = fcntl(fd, F_GETFL) | O_NONBLOCK;
-    if (fcntl(fd, F_SETFL, flags) < 0) {
-      error("Can't set UDP socket %d to nonblocking mode: %s", fd, strerror(errno));
+    flags = fcntl(fd_, F_GETFL) | O_NONBLOCK;
+    if (fcntl(fd_, F_SETFL, flags) < 0) {
+      error("Can't set UDP socket %d to nonblocking mode: %s", fd_, strerror(errno));
       goto err;
     }
 
     memset((char*)&if_addr, 0, sizeof(if_addr));
     if_addr.sin_family = AF_INET;
-    if_addr.sin_port = htons((unsigned short)port);
+    if_addr.sin_port = htons(port_);
     if_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    if (bind(fd, (struct sockaddr*)&if_addr, sizeof(if_addr)) < 0) {
-      error("Can't bind UDP socket %d to port %d: %s", fd, port, strerror(errno));
+    if (bind(fd_, (struct sockaddr*)&if_addr, sizeof(if_addr)) < 0) {
+      error("Can't bind UDP socket %d to port %d: %s", fd_, port_, strerror(errno));
       goto err;
     }
 
-    mc_group.imr_multiaddr.s_addr = inet_addr(mcastAddr);
+    mc_group.imr_multiaddr = mcastAddr_;
     mc_group.imr_interface.s_addr =
       htonl(INADDR_ANY); // inet_addr("203.106.93.94");
-    if (setsockopt(fd, IPPROTO_IP, IP_ADD_MEMBERSHIP, &mc_group,
+    if (setsockopt(fd_, IPPROTO_IP, IP_ADD_MEMBERSHIP, &mc_group,
                    sizeof(mc_group)) < 0) {
-      error("Can't add UDP socket %d to multicast group %s: %s", fd, mcastAddr,
+      error("Can't add UDP socket %d to multicast group %s: %s", fd_, mcastAddrStr_,
             strerror(errno));
       goto err;
     }
 
     info("Joined multicast group %s, listening on port %d, UDP socket %d",
-         mcastAddr, port, fd);
+         mcastAddrStr_, port_, fd_);
     return CONNECTED;
   } else if (status == DISCONNECTING) {
-    close(fd);
-    info("Disconnected UDP socket %d", fd);
-    fd = 0;
+    close(fd_);
+    info("Disconnected UDP socket %d", fd_);
+    fd_ = -1;
     return DISCONNECTED;
   }
 
@@ -79,56 +86,56 @@ NetworkStatus Udp::update(NetworkStatus status) {
   return status;
 
 err:
-  if (fd) {
-    close(fd);
-    fd = 0;
+  if (fd_ >= 0) {
+    close(fd_);
+    fd_ = -1;
   }
   error("UDP socket connection failed");
   return CONNECTION_FAILED;
 }
 
 int Udp::recv(void* buf, size_t bufsize) {
-  if (!fd) {
+  if (fd_ < 0) {
     return -1;
   }
 
   const char* fromstr = nullptr;
   sockaddr_in fromaddr;
   socklen_t fromaddrlen = sizeof(fromaddr);
-  int n = recvfrom(fd, buf, bufsize, 0,
+  int n = recvfrom(fd_, buf, bufsize, 0,
                    reinterpret_cast<sockaddr*>(&fromaddr), &fromaddrlen);
   if (n < 0) {
     if (errno == EWOULDBLOCK || errno == EAGAIN) {
       return 0; // no data on nonblocking socket
     }
-    error("Can't receive data on UDP socket %d: %s", fd, strerror(errno));
+    error("Can't receive data on UDP socket %d: %s", fd_, strerror(errno));
     goto err; // error reading
   }
   fromstr = inet_ntoa(fromaddr.sin_addr);
-  debug("Received %d bytes on UDP socket %d from %s:%d", n, fd, fromstr,
-        fromaddr.sin_port);
+  debug("Received %d bytes on UDP socket %d from %s:%d", n, fd_, fromstr,
+        ntohs(fromaddr.sin_port));
   return n;
 err:
   return -1;
 }
 
 void Udp::send(void* buf, size_t bufsize) {
-  if (!fd) {
+  if (fd_ < 0) {
     return; // -1;
   }
 
   sockaddr_in addr;
   memset((char*)&addr, 0, sizeof(addr));
   addr.sin_family = AF_INET;
-  addr.sin_port = htons((unsigned short)port);
-  addr.sin_addr.s_addr = inet_addr(mcastAddr);
-  if (static_cast<size_t>(sendto(fd, buf, bufsize, 0, (struct sockaddr*)&addr,
+  addr.sin_port = htons(port_);
+  addr.sin_addr = mcastAddr_;
+  if (static_cast<size_t>(sendto(fd_, buf, bufsize, 0, (struct sockaddr*)&addr,
                                  sizeof(addr))) != bufsize) {
-    error("Can't send %d bytes on UDP socket %d: %s", bufsize, fd,
+    error("Can't send %d bytes on UDP socket %d: %s", bufsize, fd_,
           strerror(errno));
     return; // -1;
   }
-  debug("Sent %d bytes on UDP socket %d", bufsize, fd);
+  debug("Sent %d bytes on UDP socket %d", bufsize, fd_);
   //return bufsize;
 }
 
--- extras/unisparks/networks/udp.hpp
+++ extras/unisparks/networks/udp.hpp
@@ -3,21 +3,23 @@
 #define UNISPARKS_NETWORK_UDPSOCKET_H
 #include "unisparks/network.hpp"
 
+#include <netinet/in.h>
+
 namespace unisparks {
 
 struct Udp : public Network {
   Udp(int p = DEFAULT_UDP_PORT,
-      const char* addr = DEFAULT_MULTICAST_ADDR) : mcastAddr(addr), port(p), fd(0) {
-  }
+      const char* addr = DEFAULT_MULTICAST_ADDR);
   Udp(const Udp&) = default;
 
   NetworkStatus update(NetworkStatus st) override;
   int recv(void* buf, size_t bufsize) override;
   void send(void* buf, size_t bufsize) override;
-
-  const char* mcastAddr;
-  int port;
-  int fd;
+private:
+  struct in_addr mcastAddr_;
+  char mcastAddrStr_[sizeof("255.255.255.255")];
+  const uint16_t port_;
+  int fd_;
 };
 
 } // namespace unisparks
