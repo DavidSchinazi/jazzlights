f661f8b04d7cf6ced9d8c206a50868c3e12ee106
David Schinazi <david.schinazi@gmail.com>
2022-02-05 18:45:36 -0800

    Reboot before overflowing time

--- src/unisparks/util/arduinotime.cpp
+++ src/unisparks/util/arduinotime.cpp
@@ -1,9 +1,18 @@
 #include "unisparks/util/time.hpp"
 #include <Arduino.h>
+#include <limits>
+#include <stdlib.h>
+
 namespace unisparks {
 
 Milliseconds timeMillis() {
-  return ::millis();
+  const unsigned long unsignedTime = ::millis();
+  if (unsignedTime > static_cast<unsigned long>(std::numeric_limits<Milliseconds>::max() - 3600000)) {
+    // Reboot if we get within one hour of overflowing our Milliseconds type.
+    // This happens after 24 days and 19 hours.
+    abort();
+  }
+  return static_cast<Milliseconds>(unsignedTime);
 }
 
 void delay(Milliseconds duration) {
