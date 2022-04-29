d266d7322ba43aa8697b22e42423265874a79fee
David Schinazi <david.schinazi@gmail.com>
2022-04-28 23:19:21 -0700

    rev3

--- src/unisparks/config.h
+++ src/unisparks/config.h
@@ -42,7 +42,7 @@
 #endif // BOOT_NAME
 
 #ifndef REVISION
-#  define REVISION 2
+#  define REVISION 3
 #endif // REVISION
 
 // Extra indirection ensures preprocessor expands macros in correct order.
