f6350d2029d7c46b4ae53663cf3ffe6d29591838
David Schinazi <david.schinazi@gmail.com>
2022-05-11 11:18:24 -0700

    Add sender.py

---
+++ extras/scripts/sender.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+
+# Test program that can send updates over IP multicast.
+
+import socket
+import time
+import struct
+
+PORT = 0xDF0D
+MCADDR = '239.255.223.01'
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+s.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 3)
+
+# 1 byte set to 0
+# 6 byte originator MAC address
+# 6 byte sender MAC address
+# 2 byte precedence
+# 4 byte current pattern
+# 4 byte next pattern
+# 2 byte elapsed time
+
+BLACK = 0 * 256
+RED = 1
+GREEN = 2 * 256
+BLUE = 3 * 256
+PURPLE = 4 * 256
+CYAN = 5 * 256
+YELLOW = 6 * 256
+WHITE = 7 * 256
+GLOWRED = 8 * 256
+GLOWGREEN = 9 * 256
+GLOWBLUE = 10 * 256
+GLOWPURPLE = 11 * 256
+GLOWCYAN = 12 * 256
+GLOWYELLOW = 13 * 256
+GLOWWHITE = 14 * 256
+SYNCTEST = 15 * 256
+CALIBRATION = 16 * 256
+
+typeByte = 0
+thisDeviceMacAddress = b'\xFF\xFF\x01\x02\x03\x04'
+precedence = 3000
+currentPattern = PURPLE
+nextPattern = currentPattern
+startTime = time.time()
+
+while True:
+    elapsedTime = int((time.time() - startTime) * 1000) % 10000
+    messageToSend = struct.pack('!B6s6sHIIH', typeByte, thisDeviceMacAddress, thisDeviceMacAddress, precedence, currentPattern, nextPattern, elapsedTime)
+    # print(messageToSend)
+    s.sendto(messageToSend, (MCADDR, PORT))
+    time.sleep(0.1)
