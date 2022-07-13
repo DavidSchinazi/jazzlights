c8ec208caa1cd82911f034a00180bd2ce69cf747
David Schinazi <david.schinazi@gmail.com>
2022-07-12 21:21:33 -0700

    Make sender.py pick pattern from arguments

--- extras/scripts/sender.py
+++ extras/scripts/sender.py
@@ -3,8 +3,45 @@
 # Test program that can send updates over IP multicast.
 
 import socket
-import time
 import struct
+import sys
+import time
+
+name = ''
+if len(sys.argv) >= 2:
+    name = sys.argv[1].lower()
+
+patterns = {
+    'black': 0x000,
+    'red': 0x100,
+    'green': 0x200,
+    'blue': 0x300,
+    'purple': 0x400,
+    'cyan': 0x500,
+    'yellow': 0x600,
+    'white': 0x700,
+    'glowred': 0x800,
+    'glowgreen': 0x900,
+    'glowblue': 0xA00,
+    'glowpurple': 0xB00,
+    'glowcyan': 0xC00,
+    'glowyellow': 0xD00,
+    'glowwhite': 0xE00,
+    'synctest': 0xF00,
+    'calibration': 0x1000,
+    'sp_rainbow': 0xE0000001,
+    'sp_forest': 0xD0000001,
+    'sp_party': 0xC0000001,
+    'sp_cloud': 0xB0000001,
+    'sp_ocean': 0xA0000001,
+    'sp_lava': 0x90000001,
+    'sp_heat': 0x80000001,
+    'flame': 0x60000001,
+    'glitter': 0x40000001,
+    'threesine': 0x20000001,
+    'rainbow': 0x00000001,
+}
+pattern = patterns.get(name, patterns['glowred'])
 
 PORT = 0xDF0D
 MCADDR = '239.255.223.01'
@@ -22,41 +59,12 @@ s.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 3)
 # 4 byte next pattern
 # 2 byte time delta since start of current pattern
 
-BLACK = 0 * 256
-RED = 1 * 256
-GREEN = 2 * 256
-BLUE = 3 * 256
-PURPLE = 4 * 256
-CYAN = 5 * 256
-YELLOW = 6 * 256
-WHITE = 7 * 256
-GLOWRED = 8 * 256
-GLOWGREEN = 9 * 256
-GLOWBLUE = 10 * 256
-GLOWPURPLE = 11 * 256
-GLOWCYAN = 12 * 256
-GLOWYELLOW = 13 * 256
-GLOWWHITE = 14 * 256
-SYNCTEST = 15 * 256
-CALIBRATION = 16 * 256
-
-SP_RAINBOW = 0xE0000001
-SP_FOREST = 0xD0000001
-SP_PARTY = 0xC0000001
-SP_CLOUD = 0xB0000001
-SP_OCEAN = 0xA0000001
-SP_LAVA = 0x90000001
-SP_HEAT = 0x80000001
-FLAME = 0x60000001
-GLITTER = 0x40000001
-THREESINE = 0x20000001
-RAINBOW = 0x00000001
 
 versionByte = 0x10
 thisDeviceMacAddress = b'\xFF\xFF\x01\x02\x03\x04'
 precedence = 40000
 numHops = 0
-currentPattern = GLOWRED
+currentPattern = pattern
 nextPattern = currentPattern
 startTime = time.time()
 
