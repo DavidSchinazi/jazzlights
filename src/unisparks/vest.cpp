1a5dc097ac0dc72277b823198af91dbd0c57e51a
David Schinazi <david.schinazi@gmail.com>
2022-08-07 16:10:36 -0700

    fix ropelight colors

--- src/unisparks/vest.cpp
+++ src/unisparks/vest.cpp
@@ -94,8 +94,11 @@ void vestSetup(void) {
   // that the YL inscription is on the male end of the three-way power-injection splitter.
   mainVestController = &FastLED.addLeds</*CHIPSET=*/WS2801, /*DATA_PIN=*/26, /*CLOCK_PIN=*/32, /*RGB_ORDER=*/GBR>(
     leds, sizeof(leds)/sizeof(*leds));
-#elif IS_STAFF || ROPELIGHT
+#elif IS_STAFF
   mainVestController = &FastLED.addLeds<WS2811, LED_PIN, RGB>(
+    leds, sizeof(leds)/sizeof(*leds));
+#elif ROPELIGHT
+  mainVestController = &FastLED.addLeds<WS2811, LED_PIN, BRG>(
     leds, sizeof(leds)/sizeof(*leds));
 #else  // Vest.
   mainVestController = &FastLED.addLeds<WS2812B, LED_PIN, GRB>(
