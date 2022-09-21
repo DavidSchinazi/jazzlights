08af61fc35e5f0435924de13e2bd3244c6efa88b
David Schinazi <david.schinazi@gmail.com>
2022-09-20 18:31:33 -0700

    Remove unnecessary parentheses around type in rust

--- tglight/src/webui/mod.rs
+++ tglight/src/webui/mod.rs
@@ -13,7 +13,7 @@ struct Server {
 
 impl Handler for Server {
     //
-    fn on_request(&mut self, req: &Request) -> Result<(Response)> {
+    fn on_request(&mut self, req: &Request) -> Result<Response> {
         // Using multiple handlers is better (see router example)
         match req.resource() {
             // The default trait implementation
