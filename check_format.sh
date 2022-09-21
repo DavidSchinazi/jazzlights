c1a14a075fea9b7b6f9a31a693abc91db9aa03c9
David Schinazi <david.schinazi@gmail.com>
2022-09-20 20:28:12 -0700

    Add --fix option to clang_format.sh

--- check_format.sh
+++ check_format.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
+# Set current working directory to the project directory.
 cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null
+
+FIX="$([ "$#" -ge "1" ] && [ "$1" == "--fix" ] && echo 1 || echo 0)"
 
 RET_VAL=0
 for f in $(echo "$(find . -name '*.h' ; find . -name '*.cpp')" | grep -v /.pio/ | grep -v /build/) ; do
-  if [ -n "$(clang-format --output-replacements-xml "$f" | grep '<replacement ')" ] ; then
-    echo "Format issue detected in $f"
-    RET_VAL=1
+  if [ "$FIX" == "1" ] ; then
+    clang-format -i "$f"
+  else
+    if [ -n "$(clang-format --output-replacements-xml "$f" | grep '<replacement ')" ] ; then
+      echo "Format issue detected in $f"
+      RET_VAL=1
+    fi
   fi
 done
 exit $RET_VAL
