e7d80541e4077cbe17f8e992516cb70b8add36fd
David Schinazi <david.schinazi@gmail.com>
2023-08-07 22:15:02 -0700

    Refactor tweak-robot-layout to allow multiple segments per strand

--- tglight/etc/other/tweak-robot-layout.py
+++ tglight/etc/other/tweak-robot-layout.py
@@ -11,82 +11,95 @@ startUp = True
 strands = [
   {
     'comment': 'Robot outside wall',
-    'maxX': robotWallMaxX,
-    'maxY': 1.72,
-    'minY': 0.0,
-    'numX': 24,
-    'numY': 7,
-    'startMidWay': False,
-    'specialStart': True,
     'controller': 10102,
     'strip': 0,
+    'segments': [
+      {
+        'maxX': robotWallMaxX,
+        'maxY': 1.72,
+        'minY': 0.0,
+        'numX': 24,
+        'numY': 7,
+        'startMidWay': False,
+        'specialStart': True,
+      },
+    ]
     # x = [7.5 .. 13.9] with diff 0.3 
     # on lower y =  [0.2 .. 1.72] with diff 0.25
     # on higher y = [0.0 .. 1.52] with diff 0.25
   },
   {
     'comment': 'Robot head',
-    'maxX': robotWallMaxX + 5.2,
-    'maxY': 1.38,
-    'minY': 1.0,
-    'numX': 17,
-    'numY': 2,
-    'startMidWay': True,
-    'specialStart': False,
     'controller': 10102,
     'strip': 1,
+    'segments': [
+      {
+        'maxX': robotWallMaxX + 5.2,
+        'maxY': 1.38,
+        'minY': 1.0,
+        'numX': 17,
+        'numY': 2,
+        'startMidWay': True,
+        'specialStart': False,
+      },
+    ]
     # x = [14.3 .. 16.59] with diff 0.15
     # on lower y =  [1.0  .. 1.14] with diff 0.14
     # on higher y = [1.07 .. 1.21] with diff 0.14
   },
   {
     'comment': 'Caboose outside wall new 12V WS2801',
-    'maxX': 6.6,
-    'maxY': 1.72,
-    'minY': 0.0,
-    'numX': 10,
-    'numY': 6,
-    'startMidWay': True,
-    'specialStart': False,
     'controller': 10106,
     'strip': 0,
+    'segments': [
+      {
+        'maxX': 6.6,
+        'maxY': 1.72,
+        'minY': 0.0,
+        'numX': 10,
+        'numY': 6,
+        'startMidWay': True,
+        'specialStart': False,
+      },
+    ]
   },
 ]
 
 for strand in strands:
-  maxX = strand['maxX']
-  maxY = strand['maxY']
-  minY = strand['minY']
-  numX = strand['numX']
-  numY = strand['numY']
-  startMidWay = strand['startMidWay']
-
   l = []
-  x = maxX
-  if startMidWay:
-    minY += yDiff/2
-    maxY -= yDiff/2
-  y = maxY if startUp else minY
+  for segment in strand['segments']:
+    maxX = segment['maxX']
+    maxY = segment['maxY']
+    minY = segment['minY']
+    numX = segment['numX']
+    numY = segment['numY']
+    startMidWay = segment['startMidWay']
 
-  for xi in range(numX):
-    thisMinY = minY
-    thisMaxY = maxY
-    up = (xi % 2 == (0 if startUp else 1))
-    maxyi = numY
-    if strand['specialStart']:
-      if xi <= 2:
-        maxyi = 3
-      if xi == 0:
-        thisMinY = 1.26
-    yMult = -1.0 if up else 1.0
-    for yi in range(maxyi):
-      l.append((x, y))
-      y += yDiff * yMult
-    if up:
-      y = thisMinY
-    else:
-      y = thisMaxY
-    x -= xDiff
+    x = maxX
+    if startMidWay:
+      minY += yDiff/2
+      maxY -= yDiff/2
+    y = maxY if startUp else minY
+
+    for xi in range(numX):
+      thisMinY = minY
+      thisMaxY = maxY
+      up = (xi % 2 == (0 if startUp else 1))
+      maxyi = numY
+      if segment['specialStart']:
+        if xi <= 2:
+          maxyi = 3
+        if xi == 0:
+          thisMinY = 1.26
+      yMult = -1.0 if up else 1.0
+      for yi in range(maxyi):
+        l.append((x, y))
+        y += yDiff * yMult
+      if up:
+        y = thisMinY
+      else:
+        y = thisMaxY
+      x -= xDiff
 
   s = '# {name} ({num} LEDs)\n'.format(name=strand['comment'], num=len(l)*2)
   s += '[[strand]]\n'
