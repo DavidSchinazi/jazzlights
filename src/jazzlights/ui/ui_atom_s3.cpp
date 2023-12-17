11659f82285a9d4881bfba019a70086dc96d0416
David Schinazi <david.schinazi@gmail.com>
2023-12-16 19:59:25 -0800

    Add missing ifdef

--- src/jazzlights/ui/ui_atom_s3.cpp
+++ src/jazzlights/ui/ui_atom_s3.cpp
@@ -1,5 +1,7 @@
 #include "jazzlights/ui/ui_atom_s3.h"
 
+#ifdef ARDUINO
+
 namespace jazzlights {
 
 AtomS3Ui::AtomS3Ui(Player& player, Milliseconds currentTime) : ArduinoUi(player, currentTime) {}
@@ -9,3 +11,5 @@ void AtomS3Ui::FinalSetup(Milliseconds /*currentTime*/) {}
 void AtomS3Ui::RunLoop(Milliseconds /*currentTime*/) {}
 
 }  // namespace jazzlights
+
+#endif  // ARDUINO
