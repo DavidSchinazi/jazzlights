4908cf43482c7e4ac1c45ed9af9db2cd20f4f465
David Schinazi <david.schinazi@gmail.com>
2024-02-20 00:10:23 -0800

    Remove unused branch in HSV conversion

--- src/jazzlights/util/color.cpp
+++ src/jazzlights/util/color.cpp
@@ -1,64 +1,10 @@
 #include "jazzlights/util/color.h"
 
 #include "jazzlights/util/math.h"
-#define FAST_HSL 1
 
 namespace jazzlights {
 
 Color RgbColorFromHsv(uint8_t h, uint8_t s, uint8_t v) {
-#ifndef FAST_HSL
-  double in_h = 360 * h / 255.0, in_s = s / 255.0, in_v = v / 255.0;
-  double hh, p, q, t, ff;
-  long i;
-  double out_r, out_g, out_b;
-
-  if (in_s <= 0.0) { return Color(255 * in_v, 255 * in_v, 255 * in_v); }
-  hh = in_h;
-  if (hh >= 360.0) { hh = 0.0; }
-  hh /= 60.0;
-  i = (long)hh;
-  ff = hh - i;
-  p = in_v * (1.0 - in_s);
-  q = in_v * (1.0 - (in_s * ff));
-  t = in_v * (1.0 - (in_s * (1.0 - ff)));
-
-  switch (i) {
-    case 0:
-      out_r = in_v;
-      out_g = t;
-      out_b = p;
-      break;
-    case 1:
-      out_r = q;
-      out_g = in_v;
-      out_b = p;
-      break;
-    case 2:
-      out_r = p;
-      out_g = in_v;
-      out_b = t;
-      break;
-
-    case 3:
-      out_r = p;
-      out_g = q;
-      out_b = in_v;
-      break;
-    case 4:
-      out_r = t;
-      out_g = p;
-      out_b = in_v;
-      break;
-    case 5:
-    default:
-      out_r = in_v;
-      out_g = p;
-      out_b = q;
-      break;
-  }
-
-  return Color(255 * out_r, 255 * out_g, 255 * out_b);
-#else
   uint8_t region, remainder, p, q, t, rgb_r, rgb_g, rgb_b;
 
   if (s == 0) { return Color(v, v, v); }
@@ -103,7 +49,6 @@ Color RgbColorFromHsv(uint8_t h, uint8_t s, uint8_t v) {
       break;
   }
   return Color(rgb_r, rgb_g, rgb_b);
-#endif
 }
 
 }  // namespace jazzlights
