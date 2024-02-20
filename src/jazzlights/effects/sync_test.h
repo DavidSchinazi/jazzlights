7f812afacf3c26499d5cae06a26618530eae30b0
David Schinazi <david.schinazi@gmail.com>
2024-02-19 23:12:01 -0800

    tweak in preparation for refactor

--- src/jazzlights/effects/sync_test.h
+++ src/jazzlights/effects/sync_test.h
@@ -7,7 +7,7 @@ namespace jazzlights {
 
 inline FunctionalEffect sync_test() {
   return effect("synctest", [](const Frame& frame) {
-    constexpr Color colors[] = {Red(), Green(), Blue(), White()};
+    static const Color colors[] = {Red(), Green(), Blue(), White()};
     const size_t index = static_cast<size_t>(frame.time / 1000) % (sizeof(colors) / sizeof(colors[0]));
     const Color color = colors[index];
     return [color](const Pixel& /*pt*/) -> Color { return color; };
