bf29ad1b289d8ffc321816371c0708a522cf4273
David Schinazi <david.schinazi@gmail.com>
2024-05-27 15:56:52 -0700

    Switch websocket.py to our own mDNS implementation

--- extras/scripts/websocket.py
+++ extras/scripts/websocket.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import asyncio
-import socket
+from resolve_mdns import resolve_mdns_async
 from sys import argv
 import websockets
 
@@ -13,17 +13,17 @@ if len(argv) > 1:
     elif cmd == "off":
         message = b"\x04"
 
-# In theory we could just pass in the hostname to websockets, but the ESP32
-# does not respond to mDNS AAAA requests and that leads websockets to hang for
-# a couple seconds before trying IPv4.
-host = "jazzlights-clouds.local"
-address = socket.gethostbyname(host)
-print("Resolved {h} to {a}".format(h=host, a=address))
-uri = "ws://{a}:80/jazzlights-websocket".format(a=address)
-print("Sending", message)
-
 
 async def websocket_client():
+    # In theory we could just pass in the hostname to websockets, but the ESP32
+    # does not respond to mDNS AAAA requests and that leads websockets to hang for
+    # a couple seconds before trying IPv4. Additionally, Home Assistant Container
+    # does not support mDNS due to limitations of its libc musl, so we bundle our own.
+    host = "jazzlights-clouds.local"
+    address = await resolve_mdns_async(host)
+    print("Resolved {h} to {a}".format(h=host, a=address))
+    uri = "ws://{a}:80/jazzlights-websocket".format(a=address)
+    print("Sending", message)
     async with websockets.connect(uri) as ws:
         await ws.send(message)
         response = await ws.recv()
