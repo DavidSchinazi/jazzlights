feb6ca51af01c651368aa4b6cd2d48aa35b37896
David Schinazi <david.schinazi@gmail.com>
2024-05-28 17:36:40 -0700

    Cleanup comments

--- extras/home-assistant/jazzlights/__init__.py
+++ extras/home-assistant/jazzlights/__init__.py
@@ -6,30 +6,15 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
 
-# TODO List the platforms that you want to support.
-# For your initial PR, limit it to 1 platform.
 PLATFORMS: list[Platform] = [Platform.LIGHT]
 
-# TODO Create ConfigEntry type alias with API object
-# Alias name should be prefixed by integration name
-# type New_NameConfigEntry = ConfigEntry[MyApi]  # noqa: F821
 
-
-# TODO Update entry annotation
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up JazzLights from a config entry."""
-
-    # TODO 1. Create API instance
-    # TODO 2. Validate the API connection (and authentication)
-    # TODO 3. Store an API object for your platforms to access
-    # entry.runtime_data = MyAPI(...)
-
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
-
     return True
 
 
-# TODO Update entry annotation
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
     return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
--- extras/home-assistant/jazzlights/config_flow.py
+++ extras/home-assistant/jazzlights/config_flow.py
@@ -1,7 +1,5 @@
 """Config flow for JazzLights."""
 
-# import my_pypi_dependency
-
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import config_entry_flow
 
@@ -11,8 +9,6 @@ from .const import DOMAIN
 async def _async_has_devices(hass: HomeAssistant) -> bool:
     """Return if there are devices that can be discovered."""
     # TODO Check if there are any devices that can be discovered in the network.
-    # devices = await hass.async_add_executor_job(my_pypi_dependency.discover)
-    # return len(devices) > 0
     return True
 
 
