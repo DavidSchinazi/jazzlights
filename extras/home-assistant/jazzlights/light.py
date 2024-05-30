bf5a3b144395e71752f6577c7083d6f19ce79fee
David Schinazi <david.schinazi@gmail.com>
2024-05-29 21:17:13 -0700

    Remove some more clutter from light

--- extras/home-assistant/jazzlights/light.py
+++ extras/home-assistant/jazzlights/light.py
@@ -3,7 +3,7 @@
 import logging
 from typing import Any
 
-from homeassistant.components.light import ColorMode, LightEntity, LightEntityFeature
+from homeassistant.components.light import ColorMode, LightEntity
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
