8025a4d84deb28a9ad1cbfbf2f68d008ed549bd1
David Schinazi <david.schinazi@gmail.com>
2024-06-17 21:06:59 -0700

    Remove Arduino dependency from time.cpp

--- src/jazzlights/util/time.cpp
+++ src/jazzlights/util/time.cpp
@@ -4,25 +4,24 @@
 
 #include <limits>
 
-#ifdef ARDUINO
-#include <Arduino.h>
-#else  // ARDUINO
+#ifdef ESP32
+#include "esp_timer.h"
+#else  // ESP32
 #include <chrono>
-#endif  // ARDUINO
+#endif  // ESP32
 
 namespace jazzlights {
 
 Milliseconds timeMillis() {
-#ifdef ARDUINO
-  const unsigned long systemTime = ::millis();
-#else   // ARDUINO
+#ifdef ESP32
+  const int64_t systemTime = esp_timer_get_time() / 1000;
+#else   // ESP32
   static auto t0 = std::chrono::steady_clock::now();
   const int64_t systemTime =
       std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - t0).count();
-#endif  // ARDUINO
+#endif  // ESP32
 
-  if (systemTime >
-      static_cast<std::remove_const<decltype(systemTime)>::type>(std::numeric_limits<Milliseconds>::max() - 3600000)) {
+  if (systemTime > static_cast<int64_t>(std::numeric_limits<Milliseconds>::max() - 3600000)) {
     // Crash if we get within one hour of overflowing our Milliseconds type.
     // This happens after 24 days and 19 hours and will cause the program to restart.
     abort();
