8770751cc451869d1bc74762586fbb66def57fe2
David Schinazi <david.schinazi@gmail.com>
2024-09-07 16:19:19 -0700

    Delete ArduinoEsp32WiFiNetwork

--- src/jazzlights/networks/wifi.h
+++ src/jazzlights/networks/wifi.h
@@ -1,7 +1,6 @@
 #ifndef JL_NETWORKS_WIFI_H
 #define JL_NETWORKS_WIFI_H
 
-#include "jazzlights/networks/arduino_esp32_wifi.h"
 #include "jazzlights/networks/arduino_esp_wifi.h"
 #include "jazzlights/networks/esp32_wifi.h"
 
