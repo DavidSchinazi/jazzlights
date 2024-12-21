6ee30ed7c134fce85a3769786f1c156e4c31e2f3
David Schinazi <david.schinazi@gmail.com>
2024-12-21 14:22:11 -0800

    Another failed ESP ethernet attempt

--- src/jazzlights/networks/esp32_ethernet.cpp
+++ src/jazzlights/networks/esp32_ethernet.cpp
@@ -373,10 +373,105 @@ Esp32EthernetNetwork::Esp32EthernetNetwork()
   if (udpPayload_ == nullptr) {
     jll_fatal("Esp32EthernetNetwork failed to allocate receive buffer of length %zu", kReceiveBufferLength);
   }
+  // Below are a collection of attempts to get W5500 working. None of them actually work though.
+  // This could be because we use esp-idf v4.4.7 since it is bundled with arduino-esp32 as of 2024-12-21.
+  // This might work better with esp-idf v5 which we could use once we drop the use of arduino-esp32.
 #if 1
-  // Initialize Ethernet driver
+  // This version was adapted from the esp-idf v4.4.7 example.
+  // https://github.com/espressif/esp-idf/blob/v4.4.7/examples/ethernet/basic/main/ethernet_example_main.c
+  InitializeNetStack();
 
-  // Start of example_eth_init.
+  // Create instance(s) of esp-netif for SPI Ethernet(s)
+  esp_netif_inherent_config_t esp_netif_config = ESP_NETIF_INHERENT_DEFAULT_ETH();
+  esp_netif_config_t cfg_spi = {.base = &esp_netif_config, .stack = ESP_NETIF_NETSTACK_DEFAULT_ETH};
+  esp_netif_t* eth_netif_spi = nullptr;
+  char if_key_str[10];
+  char if_desc_str[10];
+  char num_str[3];
+  itoa(0, num_str, 10);
+  strcat(strcpy(if_key_str, "ETH_SPI_"), num_str);
+  strcat(strcpy(if_desc_str, "eth"), num_str);
+  esp_netif_config.if_key = if_key_str;
+  esp_netif_config.if_desc = if_desc_str;
+  esp_netif_config.route_prio = 30;
+  eth_netif_spi = esp_netif_new(&cfg_spi);
+
+  // Init MAC and PHY configs to default
+  eth_mac_config_t mac_config_spi = ETH_MAC_DEFAULT_CONFIG();
+  eth_phy_config_t phy_config_spi = ETH_PHY_DEFAULT_CONFIG();
+
+  // Install GPIO ISR handler to be able to service SPI Eth modlues interrupts
+  InstallGpioIsrService();
+
+  // Init SPI bus
+  spi_device_handle_t spi_handle = nullptr;
+  spi_bus_config_t buscfg = {
+      .mosi_io_num = kEthernetPinMOSI,
+      .miso_io_num = kEthernetPinMISO,
+      .sclk_io_num = kEthernetPinSCK,
+      .quadwp_io_num = -1,
+      .quadhd_io_num = -1,
+  };
+  spi_host_device_t host_id = SPI3_HOST;
+  ESP_ERROR_CHECK(spi_bus_initialize(host_id, &buscfg, SPI_DMA_CH_AUTO));
+
+  // Configure SPI interface and Ethernet driver for specific SPI module
+  esp_eth_mac_t* mac_spi;
+  esp_eth_phy_t* phy_spi;
+  esp_eth_handle_t eth_handle_spi = nullptr;
+
+  spi_device_interface_config_t devcfg = {.command_bits = 16,  // Actually it's the address phase in W5500 SPI frame
+                                          .address_bits = 8,   // Actually it's the control phase in W5500 SPI frame
+                                          .mode = 0,
+                                          .clock_speed_hz = 20 * 1000 * 1000,
+                                          .queue_size = 20};
+
+  // Set SPI module Chip Select GPIO
+  devcfg.spics_io_num = kEthernetPinCS;
+
+  ESP_ERROR_CHECK(spi_bus_add_device(host_id, &devcfg, &spi_handle));
+  // w5500 ethernet driver is based on spi driver
+  eth_w5500_config_t w5500_config = ETH_W5500_DEFAULT_CONFIG(spi_handle);
+
+  // Set remaining GPIO numbers and configuration used by the SPI module
+  w5500_config.int_gpio_num = kEthernetPinInterrupt;
+  phy_config_spi.phy_addr = host_id;
+  phy_config_spi.reset_gpio_num = kEthernetPinReset;
+
+  mac_spi = esp_eth_mac_new_w5500(&w5500_config, &mac_config_spi);
+  phy_spi = esp_eth_phy_new_w5500(&phy_config_spi);
+
+  esp_eth_config_t eth_config_spi = ETH_DEFAULT_CONFIG(mac_spi, phy_spi);
+  ESP_ERROR_CHECK(esp_eth_driver_install(&eth_config_spi, &eth_handle_spi));
+
+  /* The SPI Ethernet module might not have a burned factory MAC address, we cat to set it manually.
+ 02:00:00 is a Locally Administered OUI range so should not be used except when testing on a LAN under your control.
+  */
+  uint8_t mac_addr[6] = {0x02, 0x00, 0x00, 0x12, 0x34, 0x56};
+  ESP_ERROR_CHECK(esp_eth_ioctl(eth_handle_spi, ETH_CMD_S_MAC_ADDR, mac_addr));
+
+  // attach Ethernet driver to TCP/IP stack
+  ESP_ERROR_CHECK(esp_netif_attach(eth_netif_spi, esp_eth_new_netif_glue(eth_handle_spi)));
+
+  // Register user defined event handers
+  // ESP_ERROR_CHECK(esp_event_handler_register(ETH_EVENT, ESP_EVENT_ANY_ID, &eth_event_handler, NULL));
+  // ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_ETH_GOT_IP, &got_ip_event_handler, NULL));
+  esp_event_handler_instance_t ethInstance;
+  esp_event_handler_instance_t ipInstance;
+  // Register event handlers on the default event loop. That runs in task "sys_evt" on core 0 (and is not
+  // configurable).
+  ESP_ERROR_CHECK(esp_event_handler_instance_register(ETH_EVENT, ESP_EVENT_ANY_ID, &EventHandler, this, &ethInstance));
+  ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT, ESP_EVENT_ANY_ID, &EventHandler, this, &ipInstance));
+
+  /* start Ethernet driver state machine */
+
+  ESP_ERROR_CHECK(esp_eth_start(eth_handle_spi));
+  jll_info("Esp32EthernetNetwork completed initialization v4.4.7");
+
+#elif 1
+  // This version was adapted from the esp-idf v5.2.2 example.
+  // https://github.com/espressif/esp-idf/blob/v5.2.2/examples/ethernet/basic/main/ethernet_example_main.c
+  // https://github.com/espressif/esp-idf/blob/v5.2.2/examples/ethernet/basic/components/ethernet_init/ethernet_init.c
 
   // Install GPIO ISR handler to be able to service SPI Eth modules interrupts
   InstallGpioIsrService();
@@ -484,7 +579,7 @@ Esp32EthernetNetwork::Esp32EthernetNetwork()
   /* start Ethernet driver state machine */
   ESP_ERROR_CHECK(esp_eth_start(eth_handle));
   ESP_ERROR_CHECK(esp_netif_init());
-  jll_info("Esp32EthernetNetwork completed initialization");
+  jll_info("Esp32EthernetNetwork completed initialization v5.2.2");
 #elif 1
   InitializeNetStack();
 
