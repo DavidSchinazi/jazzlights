2e22433462c82f9f6bb8a403b0639610653ee5c0
David Schinazi <david.schinazi@gmail.com>
2026-02-13 17:50:10 -0800

    Properly encode backslash in EscapeRawBuffer

--- src/jazzlights/util/log.cpp
+++ src/jazzlights/util/log.cpp
@@ -9,26 +9,21 @@ size_t EscapeRawBuffer(const uint8_t* input, size_t input_len, uint8_t* output, 
   size_t out_idx = 0;
   for (size_t i = 0; i < input_len; i++) {
     uint8_t c = input[i];
-    if (isprint(c) || c == '\n' || c == '\t') {
-      if (out_idx + 1 < out_size) {
-        output[out_idx++] = static_cast<uint8_t>(c);
-      } else {
-        break;
-      }
+    if (c == '\\') {
+      if (out_idx + 2 >= out_size) { break; }
+      output[out_idx++] = '\\';
+      output[out_idx++] = '\\';
+    } else if (isprint(c) || c == '\n' || c == '\t') {
+      if (out_idx + 1 >= out_size) { break; }
+      output[out_idx++] = static_cast<uint8_t>(c);
     } else if (c == 0) {
-      if (out_idx + 2 < out_size) {
-        output[out_idx++] = '\\';
-        output[out_idx++] = '0';
-      } else {
-        break;
-      }
+      if (out_idx + 2 >= out_size) { break; }
+      output[out_idx++] = '\\';
+      output[out_idx++] = '0';
     } else {
-      if (out_idx + 4 < out_size) {
-        int written = snprintf((char*)&output[out_idx], 5, "\\x%02X", c);
-        out_idx += static_cast<size_t>(written);
-      } else {
-        break;
-      }
+      if (out_idx + 4 >= out_size) { break; }
+      int written = snprintf((char*)&output[out_idx], 5, "\\x%02X", c);
+      out_idx += static_cast<size_t>(written);
     }
   }
   output[out_idx] = '\0';
