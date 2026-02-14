e8eed3597a81a3cb8dc70c38ef605872c4703c42
David Schinazi <david.schinazi@gmail.com>
2026-02-14 02:09:28 -0800

    Add COBS logs

--- src/jazzlights/util/cobs.cpp
+++ src/jazzlights/util/cobs.cpp
@@ -3,16 +3,32 @@
 #include <cstddef>
 #include <cstdint>
 
+#include "jazzlights/util/log.h"
+
+#define JL_COBS_PRINT 0
+
+#if JL_COBS_PRINT
+static constexpr size_t kCobsPrintBufferSize = 2048;
+#endif  // JL_COBS_PRINT
+
 namespace jazzlights {
 
 size_t CobsEncode(const uint8_t* inputBuffer, size_t inputLength, uint8_t* encodedOuputBuffer,
                   size_t encodedOuputBufferSize) {
+#if JL_COBS_PRINT
+  static uint8_t* cobsPrintBuffer = reinterpret_cast<uint8_t*>(malloc(kCobsPrintBufferSize));
+  EscapeRawBuffer(inputBuffer, inputLength, cobsPrintBuffer, kCobsPrintBufferSize);
+  jll_info("COBS encode: %s", cobsPrintBuffer);
+#endif  // JL_COBS_PRINT
   uint8_t code = 0x01;
   size_t inputIndex = 0;
   size_t outputCodeIndex = 0;
   size_t outputIndex = 1;
   while (inputIndex < inputLength) {
-    if (outputIndex >= encodedOuputBufferSize) { return 0; }
+    if (outputIndex >= encodedOuputBufferSize) {
+      jll_error("CobsEncode ran out of space");
+      return 0;
+    }
     if (inputBuffer[inputIndex] == 0x00) {
       encodedOuputBuffer[outputCodeIndex] = code;
       code = 0x01;
@@ -43,18 +59,36 @@ size_t CobsDecode(const uint8_t* encodedInputBuffer, size_t encodedInputLength, 
     const uint8_t code = encodedInputBuffer[inputIndex];
     inputIndex++;
     for (uint8_t i = 1; i < code; ++i) {
-      if (outputIndex >= outputBufferSize) { return 0; }
+      if (outputIndex >= outputBufferSize) {
+        jll_error("CobsDecode ran out of space 1");
+        return 0;
+      }
       outputBuffer[outputIndex] = encodedInputBuffer[inputIndex];
       outputIndex++;
       inputIndex++;
     }
     if (code < 0xFF) {
-      if (inputIndex == encodedInputLength) { return outputIndex; }
-      if (outputIndex >= outputBufferSize) { return 0; }
+      if (inputIndex == encodedInputLength) {
+#if JL_COBS_PRINT
+        static uint8_t* cobsPrintBuffer = reinterpret_cast<uint8_t*>(malloc(kCobsPrintBufferSize));
+        EscapeRawBuffer(outputBuffer, outputIndex, cobsPrintBuffer, kCobsPrintBufferSize);
+        jll_info("COBS decode1: %s", cobsPrintBuffer);
+#endif  // JL_COBS_PRINT
+        return outputIndex;
+      }
+      if (outputIndex >= outputBufferSize) {
+        jll_error("CobsDecode ran out of space 2");
+        return 0;
+      }
       outputBuffer[outputIndex] = 0x00;
       outputIndex++;
     }
   }
+#if JL_COBS_PRINT
+  static uint8_t* cobsPrintBuffer = reinterpret_cast<uint8_t*>(malloc(kCobsPrintBufferSize));
+  EscapeRawBuffer(outputBuffer, outputIndex, cobsPrintBuffer, kCobsPrintBufferSize);
+  jll_info("COBS decode2: %s", cobsPrintBuffer);
+#endif  // JL_COBS_PRINT
   return outputIndex;
 }
 
